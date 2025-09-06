cc2686e6b76335b2fedef42b2995e917357d51e8
Caroline <carolinekaram2020@gmail.com>
2025-09-06 21:19:32 +0300

    second comment

--- index.js
+++ index.js
@@ -395,6 +395,222 @@ var w = 20
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Day 2
+
+//for , while , do while
+
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+
+// var i= 1;
+// // do while
+// do{
+
+// console.log("");
+//     i++;
+
+// } while(i<=5);
+
+//     // while
+
+
+
+
+//switch
+
+// var color= prompt("please enter your color")
+
+
+// if(temp>20){
+// console.log("hot");
+
+// }else{
+//     console.log("cold");
+    
+// }
+
+//  var color = prompt("Enter your color")
+
+// if(color==null){
+//     alert("Thanks")
+// }else{
+
+//     switch(color){
+
+//         case "red":
+//             document.writeln("<h1 style='color:red'>Hello Js</h1>")
+//             break;
+//         case "green":
+//             document.writeln("<h1 style='color:green'>Hello Js</h1>")
+//             break;
+//         default:
+//             alert("Please enter a valid color")
+//     }
+// }
+
+
+
+//////////////////////////////////////////////////////////////////////
+
+// "use strict"
+
+// var x=10
+
+// console.log(x);
+
+// var undefined=5
+
+// var Infinity=5
+
+
+// function sum(a=5,b=9){
+// "use strict"
+// a=a||5
+// b=b||9
+
+// console.log(a+b);
+
+// }
+
+
+// sum(2,3)
+
+// sum()
+ 
+////////////////////////////////////////////////////////////////////////////////////////////////
+//String
+
+//literal creation
+
+// var str1="caro"   //string
+
+//constructor creation
+// var str2=new String("Ali") //object
+// var name2 = new String ("loda")
+
+// console.log(typeof name1);
+// console.log(typeof name2);
+
+
+// var name1 = "Hello Mostafa in our site"
+
+
+// console.log(name1.length);
+
+// console.log(name1.charAt(2)); 
+
+// console.log(name1.indexOf("a")); 
+// console.log(name1.lastIndexOf("a"));
+// console.log(name1.substring(6,14));
+// console.log(name1.split(" "));
+// var x =name1.split(" ")
+// console.log(typeof x);
+
+
+////////////////////////////////////////////////////////////////////////////
+
+// console.log(Math.E);
+
+// console.log(Math.PI);
+
+// console.log(Math.max(100,5,50,75,80)); 
+// console.log(Math.min(2,8,60,100,18,6));
+
+// console.log(Math.sqrt(16)); 
+
+// console.log(Math.pow(4,2));
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+//Array
+
+//literal creation
+
+// var arr1=[10,"Caro",true,50,[1,2,3],function test(){console.log("test");}]
+
+// console.log(arr1);
+
+//constructor creation
+
+// var arr2=new Array(10,true,80,"Hend");
+
+
+// var arr3=[10,20,30,40]
+
+
+// console.log(arr3.length);
+
+//  console.log(arr3.push(60,1));  ====>add ele last index
+// arr3.push(60,1)
+// arr3.unshift("test") =====> add ele in first index
+// console.log(arr3.unshift("test")); 
+
+
+// console.log(arr3.pop()); ======> remove in last index
+// arr3.shift() =====>remove in frist index
+
+// console.log(arr3.shift());
+
+// console.log(arr3.join(" ")); 
+
+// console.log(arr3.reverse()); 
+
+
+// sort
+// var arr1 = [50,80,20,10,5,15]
+// console.log(arr1.sort(function(a,b){
+//                  return b-a
+// }));
+
+
+// var arr1 = [2000, 1995, 1996, 2005, 2001];
+
+// // // map
+// var result3 = arr1.map(function(item){
+//     return item*2
+// })
+// console.log(result3);
+
+// // // filter
+// var result = arr1.filter(function (item) {
+//   return item > 2000;
+// });
+// console.log(result);
+
+// var res = arr1.filter(function(ele){
+//     return ele < 2000
+// })
+// console.log(res);
+
+// // // find
+// var result2 = arr1.find(function(item){
+//     return item< 2000
+//    })
+//    console.log(result2);
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+// ---------- Date 
+
+// let d = new Date()
+// console.log(d);
+
+
+// let d2 = new Date()
+// console.log(d2.toDateString());
+
+// let d3 = new Date()
+// console.log(d3.toTimeString());
+
+// var d4=new Date("2009/9/9")
 
 
 
